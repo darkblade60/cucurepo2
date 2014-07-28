4624060387eeab07c353dced01842b05e6ee4b4d
Juan Carlos <juancarlosmanises@gmail.com>
2014-07-28 12:32:56 +0200

    index cambio

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ define('ROOT', dirname(dirname(__FILE__)));
 $url = isset($_GET['url']) ? $_GET['url'] : null ;
 
 require_once (ROOT . DS . 'config' . DS . 'config.php');
+
 require_once( ROOT . DS . 'library' . DS . 'router.php');
 
 
